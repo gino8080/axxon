d646d23c1693e40ea55d636596a789a93bf762c8
Magico80 <magie.cannizzaro@gmail.com>
2019-05-25 19:30:24 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const url = 'axxon/live/media/ENELPOC/DeviceIpint.10/SourceEndpoint.video:0:0?w=1536&h=920'
+var express = require('express')
+var proxy = require('http-proxy-middleware')
+
+var app = express()
+
+app.use(
+	'/',
+	proxy({
+		target: 'http://5.249.128.21/',
+		//changeOrigin: true,
+		auth: 'root:root'
+	})
+)
+app.listen(3000)
