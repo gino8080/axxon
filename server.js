c14b3b80d5dedc6fc1760a9b7b9dc7ab76e12961
Magico80 <magie.cannizzaro@gmail.com>
2019-05-26 10:48:49 +0200

    axxon e polisnet proxy

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const url = 'axxon/live/media/ENELPOC/DeviceIpint.10/SourceEndpoint.video:0:0?w=1536&h=920'
+const MAIN_URL = 'https://progetti.polis-net.it/'
+//https://progetti.polis-net.it/codice/sensoripompei/api/web/v1/sensoriced/getdiscodati?minuti=30
+//https://dashboard.heroku.com/apps/axxon/
 var express = require('express')
 var proxy = require('http-proxy-middleware')
 
@@ -9,11 +12,23 @@ app.get('/test', (req, res) => {
 	res.json({ status: 'ok' })
 })
 app.use(
-	'/',
+	'/axxon',
 	proxy({
 		target: 'http://5.249.128.21/',
 		//changeOrigin: true,
 		auth: 'root:root'
+	})
+)
+app.use(
+	'/',
+	proxy({
+		target: 'https://5.249.128.24/',
+		logLevel: 'debug',
+		secure: false
+		/*	prependPath: false,
+		followRedirects: true*/
+		//changeOrigin: false
+		//auth: 'root:root'
 	})
 )
 
